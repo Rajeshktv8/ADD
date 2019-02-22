9a82b014a9855deb339bf4f569bac8e0090754ab
rajes <rajes@DESKTOP-P9TPCO1>
2019-02-22 13:19:41 -0500

    first commit

---
+++ Add java project/src/javaadd.java
@@ -0,0 +1,15 @@
+
+public class javaadd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b,c;
+a = 5;
+b = 6;
+c = a+b;
+System.out.println(c);
+
+ 
+	}
+
+}
